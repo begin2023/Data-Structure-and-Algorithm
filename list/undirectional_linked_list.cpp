94767ab48935a46881cb5cecd3b834ce357fc56b
w30045150 <wuyong56@huawei.com>
2023-10-09 10:31:33 +0800

    list

---
+++ list/undirectional_linked_list.cpp
@@ -0,0 +1,18 @@
+// 单项链表，包括头插，尾插，带头结点，不带头结点，指定值增删改节点，指定地址增删改节点
+
+template <typename T>
+struct UList {
+    T data;
+    UList<T>* next;
+};
+
+// 头插法建立链表
+template <typename T, size_t N>
+void EstByHead(T* datas) noexcept
+{
+
+}
+
+
+int main()
+{}
