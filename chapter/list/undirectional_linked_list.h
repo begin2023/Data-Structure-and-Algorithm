7608e6e42cbeedb6c34b6b745c5f020ff6330dac
w30045150 <wuyong56@huawei.com>
2023-10-10 09:56:47 +0800

    cursion

--- chapter/list/undirectional_linked_list.h
+++ chapter/list/undirectional_linked_list.h
@@ -1,18 +1,49 @@
 // 单项链表，包括头插，尾插，带头结点，不带头结点，指定值增删改节点，指定地址增删改节点
 
+#ifndef UNDIRECTIONAL_LINKED_LIST_H
+#define UNDIRECTIONAL_LINKED_LIST_H
+
 template <typename T>
-struct UList {
+struct Node {
     T data;
-    UList<T>* next;
+    Node *next;
 };
 
-// 头插法建立链表
+using NodePtr = Node*;
+// 头插法链表
 template <typename T, size_t N>
-void EstByHead(T* datas) noexcept
-{
+class HeadList {
+public:
+    HeadList(T t[N])
+    {}
 
-}
+    auto GetList() noexcept -> (Node *)&
+    {
+        return head;
+    }
 
+    auto GetSize() noexcept -> size_t
+    {
+        return size;
+    }
 
-int main()
-{}
+    auto InsertFront(T t) noexcept -> bool
+    {}
+
+    auto InsertBack(T t) noexcept -> bool
+    {}
+
+    auto InsertPos(T t, size_t pos) noexcept -> bool
+    {}
+
+    auto RemoveByNode(Node *node) noexcept -> bool
+    {}
+
+    auto RemoveByValue(T t) noexcept -> bool
+    {}
+
+private:
+    Node *head;
+    size_t size;
+};
+#endif
