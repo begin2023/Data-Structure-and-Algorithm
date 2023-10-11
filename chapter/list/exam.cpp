d91e247de20daf7166c285f8ad6f96019ef5211f
w30045150 <wuyong56@huawei.com>
2023-10-11 17:59:45 +0800

    list

--- chapter/list/exam.cpp
+++ chapter/list/exam.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include "undirectional_linked_list.h"
+
+
 // 如何基于链表实现LRU缓存淘汰算法？
 // 我的思路是这样的：我们维护一个有序单链表，越靠近链表尾部的结点是越早之前访问的。当有一个新的数据被访问时，我们从链表头开始顺序遍历链表。
 // 1.如果此数据之前已经被缓存在链表中了，我们遍历得到这个数据对应的结点，并将其从原来的位置删除，然后再插入到链表的头部。
@@ -20,8 +24,11 @@
 
 // 求链表的中间结点
 
-#include "undirectional_linked_list.h"
-
 
 int main()
-{}
+{
+    // 单项链表反转
+    std::vector<int> v = {1, 2, 3, 4};
+    List::TailList<int> tl(v);
+
+}
